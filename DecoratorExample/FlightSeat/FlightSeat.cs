51dfa604f76c200c2088d7d87b8ff6fea173d006
Tsvetan Manev <tsvetanmanev@gmail.com>
2016-10-11 17:47:16 +0300

    Included Decorator Example with Flight Seat classes

---
+++ DecoratorExample/FlightSeat/FlightSeat.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DecoratorExample.FlightSeat
+{
+    public class FlightSeat : IFlightSeat
+    {
+        public const int StandardPrice = 500;
+        public int GetCost()
+        {
+            var seatCost = StandardPrice;
+            return seatCost;
+        }
+
+        public string GetFacilities()
+        {
+            string seatItself = "Cabin Seat";
+            return seatItself;
+        }
+    }
+}
